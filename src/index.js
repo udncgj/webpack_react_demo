5c2b1f55af75a5d0315caac8ab7bb59d15597d59
udncgj <610445206@qq.com>
2018-03-30 20:01:34 +0800

    '3.30'

--- src/index.js
+++ src/index.js
@@ -11,43 +11,43 @@ let store = createStore(todoApp);
 
 // let rootElement = document.getElementById('root')
 render(
-  <Provider store={store}>
-    <AppRouter />
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>,
+    document.getElementById('root')
 )
-// import {  } from "module";
+// import {   } from "module";
 // import 'whatwg-fetch';
 import request from './utils/request'
 if(self.fetch) {
-  console.log('fetch');
-  // run my fetch request here
+    console.log('fetch');
+    // run my fetch request here
 } else {
-  console.log('notfetch');
-  // do something with XMLHttpRequest?
+    console.log('notfetch');
+    // do something with XMLHttpRequest?
 }
 // request('/api/login?name=sdfe&password=23211', {method:'POST'})
-//   .then((data)=>{console.log(data)})
+//     .then((data)=>{console.log(data)})
 // let url = '/api/login?name=&password=23211';
 // (async()=>{
-//   try {
-//     let data = await request('/api/login?name=&password=23211', {method:'POST'});//.then((data)=>{console.log(data)});
-//     // let data = response.json();
-//     console.log(data);
-//   } catch(e) {
-//     console.log("Oops, error", e);
-//   }
+//     try {
+//         let data = await request('/api/login?name=&password=23211', {method:'POST'});//.then((data)=>{console.log(data)});
+//         // let data = response.json();
+//         console.log(data);
+//     } catch(e) {
+//         console.log("Oops, error", e);
+//     }
 // })();
 
 // let p1 = request(url, {method:'POST'})
-//   // .then(data=>{console.log('p1'+data)})
-//   // .catch(e => console.log(e));
+//     // .then(data=>{console.log('p1'+data)})
+//     // .catch(e => console.log(e));
 // let p2 = request(url, {method:'POST'})
-//   // .then(data=>{console.log('p2'+data)})
-//   // .catch(e => console.log(e));
+//     // .then(data=>{console.log('p2'+data)})
+//     // .catch(e => console.log(e));
 // let p3 = request(url, {method:'POST'})
-//   // .then(data=>{console.log('p3'+data)})
-//   // .catch(e => console.log(e));
+//     // .then(data=>{console.log('p3'+data)})
+//     // .catch(e => console.log(e));
 // Promise.all([p1,p2,p3])
-//   .then(result => console.log(result))
-//   .catch(e => console.log(e));
+//     .then(result => console.log(result))
+//     .catch(e => console.log(e));
--- src/router.js
+++ src/router.js
@@ -1,104 +1,87 @@
 import React, { Component } from 'react'
 import {
-  // HashRouter,
-  // hashHistory,
-  BrowserRouter as Router,
-  BrowserHistory,
-  Route,
-  Switch,
-  // Router,
-  Redirect,
-  Link
+    // HashRouter, hashHistory, Router,
+    BrowserRouter as Router,
+    BrowserHistory,
+    Route,
+    Switch,
+    Redirect,
+    Link
 } from 'react-router-dom'
+
 import App from './containers/App/App'
 import Footer from './components/Footer/index.js'
 import Header from './components/Header/index.js'
-import Login from './routes/User/Login.js'
-import Register from './routes/User/Register.js'
+
+import Login from './routes/User/Login'
+import Error from './routes/Error/Error'
+import Personal from './routes/Personal/Personal'
+import Register from './routes/User/Register'
+
 import styles from './public.less'
 
 import historyUrl from './history';
 
 const AppRouter = () => (
-  <Router history={BrowserHistory}>
-    <div>
-      <Switch>
-        <Route path="/" component={Home}/>
-        {/* <Route component={Error}/> */}
-      </Switch>
-    </div>
-  </Router>
+    <Router history={BrowserHistory}>
+        <div>
+            <Switch>
+                <Route path="/" component={Home}/>
+                {/* <Route component={Error}/> */}
+            </Switch>
+        </div>
+    </Router>
 )
 
 class Home extends Component{
-  render(){
-    let match = this.props.match;
-    // console.log('home',this.props);
-    return (
-      <div>
-        <Header history={this.props.history} />
-        <Switch>
-          <Route path={historyUrl.home} component={Main}/>
-          <Route path={historyUrl.login} component={User}/>
-          <Route path={historyUrl.error} component={Error} />
-          {/* <Redirect from='*' to={historyUrl.error} /> */}
-          <Redirect from='/' to='/main' />
-        </Switch>
-      </div>
-    )
-  }
+    render(){
+        let match = this.props.match;
+        // console.log('home',this.props);
+        return (
+            <div>
+                <Header history={this.props.history} />
+                <Switch>
+                    <Route path={historyUrl.home} component={Main}/>
+                    <Route path={historyUrl.login} component={User}/>
+                    <Route path={historyUrl.error} component={Error} />
+                    {/* <Redirect from='*' to={historyUrl.error} /> */}
+                    <Redirect from='/' to='/main' />
+                </Switch>
+            </div>
+        )
+    }
 }
 class Main extends Component{
-  render(){
-    let match = this.props.match;
-    // console.log(match);
-    return (
-      <div>
-        <Footer history={this.props.history} />
-        <Switch>
-          {/* <Route path={match.url} component={Footer}/> */}
-          <Route exact path={match.url} component={App}/>
-          <Route path={`${match.url}/personal`} component={Personal}/>
-          <Redirect from='*' to={historyUrl.error} />
-        </Switch>
-      </div>
-    )
-  }
+    render(){
+        let match = this.props.match;
+        // console.log(match);
+        return (
+            <div>
+                <Footer history={this.props.history} />
+                <Switch>
+                    {/* <Route path={match.url} component={Footer}/> */}
+                    <Route exact path={match.url} component={App}/>
+                    <Route path={`${match.url}/personal`} component={Personal}/>
+                    <Redirect from='*' to={historyUrl.error} />
+                </Switch>
+            </div>
+        )
+    }
 }
 class User extends Component{
-  render(){
-    let match = this.props.match;
-    console.log(match);
-    return (
-      <div>
-        <Switch>
-          <Route exact path={match.url} component={Login}/>
-          <Route path={`${match.url}/Register`} component={Register}/>
-          <Redirect from='*' to={historyUrl.error} />
-        </Switch>
-      </div>
-    )
-  }
-}
-class Error extends Component{
-  render(){
-    console.log('error',this.props.history);
-    return (
-      <div>
-        <h2>404</h2>
-        <h4>not found {this.props.history.location.pathname}</h4>
-      </div>
-    )
-  }
-}
-class Personal extends Component{
-  render(){
-    return (
-      <div>
-        <h2 onClick={() => this.props.history.push('/user')}>登录/注册</h2>
-      </div>
-    )
-  }
+    render(){
+        let match = this.props.match;
+        console.log(match);
+        return (
+            <div>
+                <Switch>
+                    <Route exact path={match.url} component={Login}/>
+                    <Route path={`${match.url}/Register`} component={Register}/>
+                    <Redirect from='*' to={historyUrl.error} />
+                </Switch>
+            </div>
+        )
+    }
 }
 
 export default AppRouter;
